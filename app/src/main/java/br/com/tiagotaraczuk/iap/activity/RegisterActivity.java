d8a1074d50330fc9e86ae4a46b28a88b5806d8d0
ttaraczuk <tiago.taraczuk96@gmail.com>
2015-10-11 23:50:19 -0300

    Register activity

--- app/src/main/java/br/com/tiagotaraczuk/iap/activity/RegisterActivity.java
+++ app/src/main/java/br/com/tiagotaraczuk/iap/activity/RegisterActivity.java
@@ -1,13 +1,15 @@
-package br.com.tiagotaraczuk.iap;
+package br.com.tiagotaraczuk.iap.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import br.com.tiagotaraczuk.iap.R;
+
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
